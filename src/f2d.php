97475b2419c227d2f58f0b5798be5cc6e4619167
jsoningram@gmail.com <jasoningram@Jasons-MacBook-Pro.local>
2016-05-03 20:20:19 -0700

    Include marker for form type

--- src/f2d.php
+++ src/f2d.php
@@ -7,13 +7,14 @@
 	function form_to_disk() {
 
 		global $f2d_option;
-		date_default_timezone_set('America/Los_Angeles');
+		date_default_timezone_set( 'America/Los_Angeles' );
 
 		if ( isset( $_POST[ 'marker' ] ) && 'sweepstakes' == $_POST[ 'marker' ] ) {
 			$protected_dir = ABSPATH . trailingslashit( $f2d_option[ 'path' ] );
 			$form_data	   = trim( $_POST[ 'firstName' ] ) . ',';
 			$form_data    .= trim( $_POST[ 'lastName' ] ) . ',';
-			$form_data    .= trim( $_POST[ 'email' ] );
+			$form_data    .= trim( $_POST[ 'email' ] ) . ',';
+			$form_data    .= trim( $_POST[ 'pageId' ] );
 			$entries       = fopen( $protected_dir . $f2d_option[ 'filename' ], 'a' ) 
 				or die( 'Unable to process that request' );
 			$txt           = date( 'Ymd' );
